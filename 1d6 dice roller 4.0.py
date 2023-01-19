0e3b9c3f32ffb3f3b0a7e0078d6d253e695bc1e9
CRKrohn <123129514+CRKrohn@users.noreply.github.com>
2023-01-19 16:40:12 -0700

    Add files via upload

---
+++ 1d6 dice roller 4.0.py
@@ -0,0 +1,28 @@
+import random
+
+def RollDice(rolls):
+    for i in range(0, rolls):
+        number = random.randint(1, 6)
+        print()
+        print([number])
+        print()
+
+def Menu() :
+    choice = (0)
+    while (choice != "3") :
+        print("1. Roll a Die")
+        print("2. Roll Multiple Dice")
+        print("____________________")
+        print("3. Exit Program")
+        choice = int(input("Enter here: "))
+        print()
+
+        if(choice == 1):
+            RollDice(1)
+        if(choice == 2):
+            rolls = int(input("How many dice would you like to roll? "))
+            RollDice(rolls)
+        if(choice == 3):
+            quit()
+
+Menu()
